57714ed740e790d67e1bd7885ad1eec5177ce906
megahomyak <g.megahomyak@gmail.com>
2023-04-08 12:45:45 +0500

    Version bump

--- src/main.rs
+++ src/main.rs
@@ -124,7 +124,7 @@ fn main() {
         )
     });
     let token = format!("Bearer {}", token.trim());
-    let client = reqwest::blocking::Client::builder().timeout(None).build();
+    let client = reqwest::blocking::Client::builder().timeout(None).build().unwrap();
     let chatgpt = ChatGPT { client, token };
     match args.action {
         None => chatgpt.chat(),
